8e0a15b4b3b010c575121e321fc8f8c114a559c1
Philip Thomsen <philip-th@live.dk>
2022-12-22 18:58:35 +0100

    Project setup

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+const port = 3000
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send('hello world')
+})
+
+app.listen(port, () => {
+    console.log(`Webserver running on http://localhost:${port}`);
+})
